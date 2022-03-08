30c1ec9172d0407aa2e24ea812b9cb31a3e3f3fd
Mathias <mathias.skallerud@gmail.com>
2022-03-08 13:29:55 +0100

    Remove log and format doc

--- src/language-server/hot-drink-dsl-scope.ts
+++ src/language-server/hot-drink-dsl-scope.ts
@@ -1,50 +1,85 @@
-import { AstNode, AstNodeDescriptionProvider, DefaultScopeProvider, getContainerOfType, getDocument, LangiumServices, Scope, SimpleScope, stream } from "langium";
-import {  isComponent, isMethod, isModel } from "./generated/ast";
+import {
+  AstNode,
+  AstNodeDescriptionProvider,
+  DefaultScopeProvider,
+  getContainerOfType,
+  getDocument,
+  LangiumServices,
+  Scope,
+  SimpleScope,
+  stream,
+} from "langium";
+import { isComponent, isMethod, isModel } from "./generated/ast";
 
 export class HotDrinkDslScopeProvider extends DefaultScopeProvider {
-    descriptionProvider: AstNodeDescriptionProvider;
+  descriptionProvider: AstNodeDescriptionProvider;
 
-    constructor (services: LangiumServices) {
-        super(services)
-        this.descriptionProvider = services.index.AstNodeDescriptionProvider
+  constructor(services: LangiumServices) {
+    super(services);
+    this.descriptionProvider = services.index.AstNodeDescriptionProvider;
+  }
+
+  getScope(node: AstNode, referenceId: string): Scope {
+    if (referenceId === "VariableReference:ref") {
+      const componentNode = getContainerOfType(node, isComponent);
+      const descriptions = componentNode!.variables
+        .flatMap((v) => v.vars)
+        .map((v) =>
+          this.descriptionProvider.createDescription(v, v.name, getDocument(v))
+        );
+      return new SimpleScope(stream(descriptions));
     }
-    
 
-    getScope(node: AstNode, referenceId: string): Scope {
-        console.log(referenceId);
-        
-        
-        if (referenceId === "VariableReference:ref") {
-            const componentNode = getContainerOfType(node, isComponent);
-            const descriptions = componentNode!.variables.flatMap(v => v.vars).map(v => this.descriptionProvider.createDescription(v, v.name, getDocument(v)))
-            return new SimpleScope(stream(descriptions))
-        }
-        
-        if (referenceId === "FunctionCall:funcRef") {
-            const modelNode = getContainerOfType(node, isModel);
-            const descriptions = modelNode!.imports.flatMap(i => i.funcs).map(v => this.descriptionProvider.createDescription(v, v.name, getDocument(v)))
-            return new SimpleScope(stream(descriptions))
-        }
-
-        if(referenceId === "FunctionCall:args") {
-            const methodNode = getContainerOfType(node, isMethod);
-            const descriptions = methodNode!.signature.inputVariables.map(v => v.ref.ref).map(v => this.descriptionProvider.createDescription(v!, v!.name, getDocument(v!)))
-            return new SimpleScope(stream(descriptions))
-        }
-        if (referenceId === "Atomic:value") {
-            const methodNode = getContainerOfType(node, isMethod);
-            const descriptions = methodNode!.signature.inputVariables.map(v => v.ref.ref).map(v => this.descriptionProvider.createDescription(v!, v!.name, getDocument(v!)))
-            return new SimpleScope(stream(descriptions))
-        }
-        if(referenceId === "VarRef:value") {
-            const methodNode = getContainerOfType(node, isMethod);
-            const descriptions = methodNode!.signature.inputVariables.map(v => v.ref.ref).map(v => this.descriptionProvider.createDescription(v!, v!.name, getDocument(v!)))
-            return new SimpleScope(stream(descriptions))
-        }
-        
-        return super.getScope(node, referenceId)
-
+    if (referenceId === "FunctionCall:funcRef") {
+      const modelNode = getContainerOfType(node, isModel);
+      const descriptions = modelNode!.imports
+        .flatMap((i) => i.funcs)
+        .map((v) =>
+          this.descriptionProvider.createDescription(v, v.name, getDocument(v))
+        );
+      return new SimpleScope(stream(descriptions));
     }
-    
 
+    if (referenceId === "FunctionCall:args") {
+      const methodNode = getContainerOfType(node, isMethod);
+      const descriptions = methodNode!.signature.inputVariables
+        .map((v) => v.ref.ref)
+        .map((v) =>
+          this.descriptionProvider.createDescription(
+            v!,
+            v!.name,
+            getDocument(v!)
+          )
+        );
+      return new SimpleScope(stream(descriptions));
+    }
+    if (referenceId === "Atomic:value") {
+      const methodNode = getContainerOfType(node, isMethod);
+      const descriptions = methodNode!.signature.inputVariables
+        .map((v) => v.ref.ref)
+        .map((v) =>
+          this.descriptionProvider.createDescription(
+            v!,
+            v!.name,
+            getDocument(v!)
+          )
+        );
+      return new SimpleScope(stream(descriptions));
+    }
+    if (referenceId === "VarRef:value") {
+      const methodNode = getContainerOfType(node, isMethod);
+      const descriptions = methodNode!.signature.inputVariables
+        .map((v) => v.ref.ref)
+        .map((v) =>
+          this.descriptionProvider.createDescription(
+            v!,
+            v!.name,
+            getDocument(v!)
+          )
+        );
+      return new SimpleScope(stream(descriptions));
+    }
+
+    return super.getScope(node, referenceId);
+  }
 }
