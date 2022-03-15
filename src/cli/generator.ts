99b1ec1354a26d59def66b7fd03e688138626147
Mathias <mathias.skallerud@gmail.com>
2022-03-15 11:50:06 +0100

    Add generation js for imports

--- src/cli/generator.ts
+++ src/cli/generator.ts
@@ -1,6 +1,6 @@
 import fs from 'fs';
 import { CompositeGeneratorNode, NL, processGeneratorNode } from 'langium';
-import { Model } from '../language-server/generated/ast';
+import { Import, ImportedFunction, Model } from '../language-server/generated/ast';
 import { extractDestinationAndName } from './cli-util';
 import path from 'path';
 
@@ -10,7 +10,12 @@ export function generateJavaScript(model: Model, filePath: string, destination: 
 
     const fileNode = new CompositeGeneratorNode();
     fileNode.append('"use strict";', NL, NL);
-    // model.greetings.forEach(greeting => fileNode.append(`console.log('Hello, ${greeting.person.$refText}!');`, NL));
+    // model.imports.forEach(imports => fileNode.append(`console.log('Hello, ${imports.file}!');`, NL));
+
+    generateImports(model.imports, fileNode)
+    fileNode.append(NL)
+
+
 
     if (!fs.existsSync(data.destination)) {
         fs.mkdirSync(data.destination, { recursive: true });
@@ -18,3 +23,10 @@ export function generateJavaScript(model: Model, filePath: string, destination: 
     fs.writeFileSync(generatedFilePath, processGeneratorNode(fileNode));
     return generatedFilePath;
 }
+
+function generateImports(imports: Import[], fileNode: CompositeGeneratorNode) {
+    imports.forEach((_import: Import) => {
+        fileNode.append(`import { ${_import.funcs.map((func:ImportedFunction) => `${func.name}`).join(', ')} } from ${_import.file};`, NL)
+    })
+}
+
