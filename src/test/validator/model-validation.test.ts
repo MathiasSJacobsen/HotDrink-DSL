fd3967e7d2c87ef07341084a6f4032feb7d30326
Mathias <mathias.skallerud@gmail.com>
2022-04-18 15:36:07 +0200

    Formating changes

--- src/test/validator/model-validation.test.ts
+++ src/test/validator/model-validation.test.ts
@@ -1,4 +1,4 @@
-import { parseHelper } from "langium/lib/test"
+import { parseHelper } from "langium/lib/test";
 import { Model } from "../../language-server/generated/ast";
 import { createHotDrinkDslServices } from "../../language-server/hot-drink-dsl-module";
 import { ERRORSEVERITY, WARNINGSEVERITY } from "../test-utils";
@@ -6,72 +6,82 @@ import { ERRORSEVERITY, WARNINGSEVERITY } from "../test-utils";
 const services = createHotDrinkDslServices();
 const helper = parseHelper<Model>(services.hotdrinkDSL);
 
-
 describe("Model validation", () => {
+  describe("warnings", () => {
     describe("Function is not imported twice", () => {
-        it('gets a warning if function is imported twice', async () => {
-            const documentContent = `import { t, k, t }from "test.js"`;
-            const expectation = {
-                message: "Should not import the same function more then once.",
-                severity: ERRORSEVERITY
-            }
-                ;
-            const doc = await helper(documentContent);
-            const diagnostics = await services.hotdrinkDSL.validation.DocumentValidator.validateDocument(doc);
+      it("gets a warning if function is imported twice", async () => {
+        const documentContent = `import { t, k, t }from "test.js"`;
+        const expectation = {
+          message: "Should not import the same function more then once.",
+          severity: ERRORSEVERITY,
+        };
+        const doc = await helper(documentContent);
+        const diagnostics =
+          await services.hotdrinkDSL.validation.DocumentValidator.validateDocument(
+            doc
+          );
 
-            expect(diagnostics.length).toBe(1)
+        expect(diagnostics.length).toBe(1);
 
-            expect(diagnostics[0]).toEqual(expect.objectContaining(expectation))
-        })
-        it('gets a warning if function is imported twice, on the right line', async () => {
-            const documentContent = `import { t, k, t } from "test.js"`;
+        expect(diagnostics[0]).toEqual(expect.objectContaining(expectation));
+      });
+      it("gets a warning if function is imported twice, on the right line", async () => {
+        const documentContent = `import { t, k, t } from "test.js"`;
 
-            const doc = await helper(documentContent);
-            const diagnostics = await services.hotdrinkDSL.validation.DocumentValidator.validateDocument(doc);
+        const doc = await helper(documentContent);
+        const diagnostics =
+          await services.hotdrinkDSL.validation.DocumentValidator.validateDocument(
+            doc
+          );
 
-            expect(diagnostics.length).toBe(1)
+        expect(diagnostics.length).toBe(1);
 
-            expect(diagnostics[0]).toEqual(expect.objectContaining({
-                range: expect.objectContaining({
-                    end: expect.objectContaining({
-                        line: 0
-                    }),
-                    start: expect.objectContaining({
-                        line: 0
-                    })
-                })
-            }))
-        })
-        describe("All components should have unique names", () => {
-            it("gets a warning if two components have the same name", async () => {
-                const documentContent = `
+        expect(diagnostics[0]).toEqual(
+          expect.objectContaining({
+            range: expect.objectContaining({
+              end: expect.objectContaining({
+                line: 0,
+              }),
+              start: expect.objectContaining({
+                line: 0,
+              }),
+            }),
+          })
+        );
+      });
+      describe("All components should have unique names", () => {
+        it("gets a warning if two components have the same name", async () => {
+          const documentContent = `
                 component t {
                     var a = true, b, c;
-    
+                    
                     constraint c1 {
                         method(a, c -> b) => true;
                         (a, c -> b) => true;
-
+                        
                     }
                 }
                 component t {
-
+                    
                 }
                 `;
 
-                const expectation = {
-                    message: "Component names should be unique",
-                    severity: WARNINGSEVERITY
-                }
-    
+          const expectation = {
+            message: "Component names should be unique",
+            severity: WARNINGSEVERITY,
+          };
 
-                const doc = await helper(documentContent);
-                const diagnostics = await services.hotdrinkDSL.validation.DocumentValidator.validateDocument(doc);
-                
-                expect(diagnostics.length).toBe(1)
+          const doc = await helper(documentContent);
+          const diagnostics =
+            await services.hotdrinkDSL.validation.DocumentValidator.validateDocument(
+              doc
+            );
 
-                expect(diagnostics[0]).toEqual(expect.objectContaining(expectation))
-            })
-        })
-    })
-})
+          expect(diagnostics.length).toBe(1);
+
+          expect(diagnostics[0]).toEqual(expect.objectContaining(expectation));
+        });
+      });
+    });
+  });
+});
--- src/test/validator/variable-validation.test.ts
+++ src/test/validator/variable-validation.test.ts
@@ -1,48 +1,59 @@
 /* we expose some testing functionality in the framework, but none for validation so far. For a quick custom setup you could simply check whether the diagnostics returned from the DocumentValidator match whatever you expect.  */
 
-import { parseHelper } from "langium/lib/test"
+import { parseHelper } from "langium/lib/test";
 import { Model } from "../../language-server/generated/ast";
 import { createHotDrinkDslServices } from "../../language-server/hot-drink-dsl-module";
+import { HINTSERVERITY, WARNINGSEVERITY } from "../test-utils";
 
 const services = createHotDrinkDslServices().hotdrinkDSL;
 const helper = parseHelper<Model>(services);
 describe("Variable validation", () => {
-    describe("warnings", () => {
-
-        it('gets a warning if variable have a uppercase starting letter as name', async () => {
-            
-            const documentContent = `component T { 
+  describe("warnings", () => {
+    it("gets a warning if variable have a uppercase starting letter as name", async () => {
+      const documentContent = `component T { 
                 var A = 3; 
                 var b = 1;
                 var c = 2; 
             }`;
-            const expectation = [
-                { 
-                    message: "Var name should start with lowercase.", severity: 2 
-                },
-                { 
-                    message: 'Variable not in use.', severity: 2 
-                },
-                { 
-                    message: 'Variable not in use.', severity: 2 
-                },
-                { 
-                    message: 'Variable not in use.', severity: 2 
-                },       
-            ];
-            const doc = await helper(documentContent);
-            const diagnostics = await services.validation.DocumentValidator.validateDocument(doc);
-            
-            expect(diagnostics.length).toBe(4);
-            expect(diagnostics.pop()).toEqual(expect.objectContaining(expectation.pop()));
-            expect(diagnostics.pop()).toEqual(expect.objectContaining(expectation.pop()));
-            expect(diagnostics.pop()).toEqual(expect.objectContaining(expectation.pop()));
-            expect(diagnostics.pop()).toEqual(expect.objectContaining(expectation.pop()));
-            
-        });
-        
-        it('gets two warnings if both variables have a uppercase starting letter', async () => {
-            const documentContent = `component T { 
+      const expectation = [
+        {
+          message: "Var name should start with lowercase.",
+          severity: WARNINGSEVERITY,
+        },
+        {
+          message: "Variable not in use.",
+          severity: WARNINGSEVERITY,
+        },
+        {
+          message: "Variable not in use.",
+          severity: WARNINGSEVERITY,
+        },
+        {
+          message: "Variable not in use.",
+          severity: WARNINGSEVERITY,
+        },
+      ];
+      const doc = await helper(documentContent);
+      const diagnostics =
+        await services.validation.DocumentValidator.validateDocument(doc);
+
+      expect(diagnostics.length).toBe(4);
+      expect(diagnostics.pop()).toEqual(
+        expect.objectContaining(expectation.pop())
+      );
+      expect(diagnostics.pop()).toEqual(
+        expect.objectContaining(expectation.pop())
+      );
+      expect(diagnostics.pop()).toEqual(
+        expect.objectContaining(expectation.pop())
+      );
+      expect(diagnostics.pop()).toEqual(
+        expect.objectContaining(expectation.pop())
+      );
+    });
+
+    it("gets two warnings if both variables have a uppercase starting letter", async () => {
+      const documentContent = `component T { 
                 var A = 1; 
                 var b = 2;
                 var C = 3;
@@ -52,32 +63,40 @@ describe("Variable validation", () => {
                     
                 }
             }`;
-            const expectation = [
-                { 
-                    message: "Var name should start with lowercase.", 
-                    severity: 2 
-                }, 
-                { 
-                    message: "Var name should start with lowercase.", 
-                    severity: 2
-                },
-                { 
-                    message: "Able to make permutations", 
-                    severity: 4
-                }
-            ];
-            const doc = await helper(documentContent);
-            const diagnostics = await services.validation.DocumentValidator.validateDocument(doc);
-            
-            expect(diagnostics.length).toBe(3)
-            
-            expect(diagnostics.pop()).toEqual(expect.objectContaining(expectation.pop()));
-            expect(diagnostics.pop()).toEqual(expect.objectContaining(expectation.pop()));
-            expect(diagnostics.pop()).toEqual(expect.objectContaining(expectation.pop()));
-            
-        });
-        it('have no diagnostic errors if none there', async () => {
-            const documentContent = `component T { 
+      const expectation = [
+        {
+          message: "Var name should start with lowercase.",
+          severity: WARNINGSEVERITY,
+        },
+        {
+          message: "Var name should start with lowercase.",
+          severity: WARNINGSEVERITY,
+        },
+        {
+          message: "Able to make permutations",
+          severity: HINTSERVERITY,
+        },
+      ];
+      const doc = await helper(documentContent);
+      const diagnostics =
+        await services.validation.DocumentValidator.validateDocument(doc);
+
+      expect(diagnostics.length).toBe(3);
+
+      expect(diagnostics.pop()).toEqual(
+        expect.objectContaining(expectation.pop())
+      );
+      expect(diagnostics.pop()).toEqual(
+        expect.objectContaining(expectation.pop())
+      );
+      expect(diagnostics.pop()).toEqual(
+        expect.objectContaining(expectation.pop())
+      );
+    });
+  });
+  describe("errors", () => {
+    it("have no diagnostic errors if none there", async () => {
+      const documentContent = `component T { 
                 var a; 
                 var b;
                 var c; 
@@ -87,15 +106,16 @@ describe("Variable validation", () => {
                     (a, c -> b) => true;
                 }
             }`;
-            const doc = await helper(documentContent);
-            const diagnostics = await services.validation.DocumentValidator.validateDocument(doc);
-            
-            expect(diagnostics.every(d => d.severity !== 1)).toBe(true)
-        });
-    })
-        describe("hints", () => {
-            it('returns a hint to make the variable lowercase', async () => {
-                const documentContent = `component T { 
+      const doc = await helper(documentContent);
+      const diagnostics =
+        await services.validation.DocumentValidator.validateDocument(doc);
+
+      expect(diagnostics.every((d) => d.severity !== 1)).toBe(true);
+    });
+  });
+  describe("hints", () => {
+    it("returns a hint to make the variable lowercase", async () => {
+      const documentContent = `component T { 
                     var a; 
                     var b = true;
                     var c = false; 
@@ -106,16 +126,16 @@ describe("Variable validation", () => {
                                     }
                                 }`;
 
-            const expectation = 
-                { 
-                    message: "Able to initialize all variables to zero", 
-                    severity: HINTSERVERITY
-                }                     
-            const doc = await helper(documentContent);
-            const diagnostics = await services.validation.DocumentValidator.validateDocument(doc);
-            
-            expect(diagnostics.length).toBe(1)
-            expect(diagnostics[0]).toEqual(expect.objectContaining(expectation))
-        });
+      const expectation = {
+        message: "Able to initialize all variables to zero",
+        severity: HINTSERVERITY,
+      };
+      const doc = await helper(documentContent);
+      const diagnostics =
+        await services.validation.DocumentValidator.validateDocument(doc);
+
+      expect(diagnostics.length).toBe(1);
+      expect(diagnostics[0]).toEqual(expect.objectContaining(expectation));
     });
-})
+  });
+});
