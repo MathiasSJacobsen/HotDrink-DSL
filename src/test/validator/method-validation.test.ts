8e363374e7d21664a8be3e713c1bfee0ff5b9e1e
Mathias <mathias.skallerud@gmail.com>
2022-04-18 15:39:51 +0200

    Structure

--- src/test/validator/method-validation.test.ts
+++ src/test/validator/method-validation.test.ts
@@ -1,6 +1,6 @@
 /* we expose some testing functionality in the framework, but none for validation so far. For a quick custom setup you could simply check whether the diagnostics returned from the DocumentValidator match whatever you expect.  */
 
-import { parseHelper } from "langium/lib/test"
+import { parseHelper } from "langium/lib/test";
 import { Model } from "../../language-server/generated/ast";
 import { createHotDrinkDslServices } from "../../language-server/hot-drink-dsl-module";
 import { HINTSERVERITY, WARNINGSEVERITY } from "../test-utils";
@@ -8,27 +8,30 @@ import { HINTSERVERITY, WARNINGSEVERITY } from "../test-utils";
 const services = createHotDrinkDslServices().hotdrinkDSL;
 const helper = parseHelper<Model>(services);
 describe("Method validation", () => {
-    it('get a hint if only one method in a constraint', async () => {
-        const documentContent = `component T {
-            var a = true, b, c;
+  describe("hints", () => {
+    it("get a hint if only one method in a constraint", async () => {
+      const documentContent = `component T {
+                var a = true, b, c;
+                
+                constraint g {
+                    method(a, b -> c) => true;  
+                }
+            }`;
+      const expectation = {
+        message: "Able to make permutations",
+        severity: HINTSERVERITY,
+      };
+      const doc = await helper(documentContent);
+      const diagnostics =
+        await services.validation.DocumentValidator.validateDocument(doc);
 
-            constraint g {
-                method(a, b -> c) => true;  
-            }
-        }`;
-        const expectation = { 
-            message: "Able to make permutations", 
-            severity: HINTSERVERITY 
-        }
-        ;
-        const doc = await helper(documentContent);
-        const diagnostics = await services.validation.DocumentValidator.validateDocument(doc);
-        
-        expect(diagnostics.length).toBe(1)
-        expect(diagnostics[0]).toEqual(expect.objectContaining(expectation))
+      expect(diagnostics.length).toBe(1);
+      expect(diagnostics[0]).toEqual(expect.objectContaining(expectation));
     });
-    it('gets a warning if method name starts with uppercase letter', async () => {
-        const documentContent = `component T {
+  });
+  describe("warnings", () => {
+    it("gets a warning if method name starts with uppercase letter", async () => {
+      const documentContent = `component T {
             var a = true, b, c;
 
         
@@ -37,15 +40,16 @@ describe("Method validation", () => {
                 m(a, c -> b) => true;
             }
         }`;
-        const expectation = { 
-                message: "Methods should start with lowercase.", 
-                severity: WARNINGSEVERITY 
-            }
-        ;
-        const doc = await helper(documentContent);
-        const diagnostics = await services.validation.DocumentValidator.validateDocument(doc);
-        
-        expect(diagnostics.length).toBe(1)
-        expect(diagnostics[0]).toEqual(expect.objectContaining(expectation))
-    }) // TODO: missing validaton
-})
+      const expectation = {
+        message: "Methods should start with lowercase.",
+        severity: WARNINGSEVERITY,
+      };
+      const doc = await helper(documentContent);
+      const diagnostics =
+        await services.validation.DocumentValidator.validateDocument(doc);
+
+      expect(diagnostics.length).toBe(1);
+      expect(diagnostics[0]).toEqual(expect.objectContaining(expectation));
+    }); // TODO: missing validaton
+  });
+});
